83b1cc00bb0eb562d6ddd2d8a69ab84c33f13b00
Guillaume Turri <g.turri@criteo.com>
2025-02-26 23:36:27 +0100

    Implement min-max solver
    it should reduce the number of guesses...
    ... but it is reaaalllly slow for the first guesses (several minutes
    on my laptop)

--- mastermind-solver/Solver.cs
+++ mastermind-solver/Solver.cs
@@ -21,6 +21,44 @@ internal class Solver(bool verbose = true)
         return candidates[0];
     }
 
+    public Combination FindNextMinMaxCandidate(IEnumerable<PlayedCombination> playedCombinations)
+    {
+        var candidates = EnumeratesAllCombinations()
+            .Where(c => c.IsCandidateSolution(playedCombinations))
+            .ToArray();
+
+        if (candidates.Length == 0)
+        {
+            throw new Exception("No candidate found");
+        }
+
+        var originalPlayedCombinations = playedCombinations.ToArray();
+
+        var nextGuess = candidates
+            .Select(c => (c, ComputeWorstCaseRemainingCandidatesIfThisOneIsPicked(c, candidates, originalPlayedCombinations)))
+            .MinBy(t => t.Item2);
+
+        if (verbose)
+        {
+            Console.WriteLine($"Still {candidates.Length} candidates. After this guess we should have at most {nextGuess.Item2} candidates left");
+        }
+
+        return nextGuess.Item1;
+    }
+
+    private int ComputeWorstCaseRemainingCandidatesIfThisOneIsPicked(Combination consideredCandidate, Combination[] remainingCandidatesSoFar, PlayedCombination[] playedCombinations)
+    {
+        var worstCase = 0;
+        foreach (var solution in remainingCandidatesSoFar)
+        {
+            var nextPlayedCombination = new List<PlayedCombination>(playedCombinations) { new(consideredCandidate, consideredCandidate.ComputeResult(solution)) };
+            var nbRemainingCandidatesNextTurn = EnumeratesAllCombinations().Count(c => c.IsCandidateSolution(nextPlayedCombination));
+            worstCase = Math.Max(worstCase, nbRemainingCandidatesNextTurn);
+        }
+
+        return worstCase;
+    }
+
     public static IEnumerable<Combination> EnumeratesAllCombinations()
     {
         for (var token1=Token.BLACK; token1 <= Token.BLUE; token1++)
