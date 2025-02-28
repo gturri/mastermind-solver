ce38ea850420d6c9de2e704f636a83d9c61e856c
Guillaume Turri <g.turri@criteo.com>
2025-02-28 21:51:31 +0100

    Cache allCombinations
    This saves ~10% of time on my laptop

--- mastermind-solver/Solver.cs
+++ mastermind-solver/Solver.cs
@@ -5,7 +5,7 @@ internal class Solver(bool verbose = true)
     public Combination FindCandidate(IEnumerable<PlayedCombination> playedCombinations)
     {
         // It would be cheaper to stop at the first candidate, but I'm interested in the number of candidate left, and it's still pretty cheap anyway
-        var candidates = EnumeratesAllCombinations()
+        var candidates = AllCombinations
             .Where(c => c.IsCandidateSolution(playedCombinations))
             .ToArray();
         if (verbose)
@@ -23,7 +23,7 @@ internal class Solver(bool verbose = true)
 
     public Combination FindNextMinMaxCandidate(IEnumerable<PlayedCombination> playedCombinations)
     {
-        var candidates = EnumeratesAllCombinations()
+        var candidates = AllCombinations
             .Where(c => c.IsCandidateSolution(playedCombinations))
             .ToArray();
 
@@ -52,20 +52,25 @@ internal class Solver(bool verbose = true)
         foreach (var solution in remainingCandidatesSoFar)
         {
             var nextPlayedCombination = new List<PlayedCombination>(playedCombinations) { new(consideredCandidate, consideredCandidate.ComputeResult(solution)) };
-            var nbRemainingCandidatesNextTurn = EnumeratesAllCombinations().Count(c => c.IsCandidateSolution(nextPlayedCombination));
+            var nbRemainingCandidatesNextTurn = AllCombinations.Count(c => c.IsCandidateSolution(nextPlayedCombination));
             worstCase = Math.Max(worstCase, nbRemainingCandidatesNextTurn);
         }
 
         return worstCase;
     }
 
-    public static IEnumerable<Combination> EnumeratesAllCombinations()
+    private static readonly Lazy<Combination[]> LazyAllCombinations = new(ComputeAllCombinations);
+    public static Combination[] AllCombinations => LazyAllCombinations.Value;
+
+    private static Combination[] ComputeAllCombinations()
     {
+        var result = new List<Combination> { Capacity = 6*6*6*6 };
         for (var token1=Token.BLACK; token1 <= Token.BLUE; token1++)
         for (var token2=Token.BLACK; token2 <= Token.BLUE; token2++)
         for (var token3=Token.BLACK; token3 <= Token.BLUE; token3++)
         for (var token4=Token.BLACK; token4 <= Token.BLUE; token4++)
-            yield return new Combination(token1, token2, token3, token4);
+            result.Add(new Combination(token1, token2, token3, token4));
+        return result.ToArray();
     }
 }
 
