15ce55fc88a5941c522c13a5f38fc62e4467d4f7
Guillaume Turri <g.turri@criteo.com>
2025-02-28 22:45:05 +0100

    DirectSolver can use optimized first guess
    with this option on then in the worst case it takes 8 guesses to find
    the solution instead of 9

--- mastermind-solver/DirectSolver.cs
+++ mastermind-solver/DirectSolver.cs
@@ -1,9 +1,14 @@
 ﻿namespace mastermind_solver;
 
-internal class DirectSolver(bool verbose=true) : Solver
+internal class DirectSolver(bool verbose=true, bool useHardCodedFirstGuess=false) : Solver
 {
     public override Combination ComputeNextGuess(List<PlayedCombination> playedCombinations)
     {
+        if (useHardCodedFirstGuess && playedCombinations.Count == 0)
+        {
+            return new Combination(Token.BLACK, Token.BLACK, Token.WHITE, Token.WHITE);
+        }
+
         // It would be cheaper to stop at the first candidate, but I'm interested in the number of candidate left, and it's still pretty cheap anyway
         var candidates = AllCombinations
             .Where(c => c.IsCandidateSolution(playedCombinations))
