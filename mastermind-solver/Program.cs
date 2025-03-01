b2e6f65df9dfafbd430e017f5bd66f5df4a0642c
Guillaume Turri <g.turri@criteo.com>
2025-03-01 22:35:15 +0100

    Compute not only the worst case but all the histogram

--- mastermind-solver/Program.cs
+++ mastermind-solver/Program.cs
@@ -2,7 +2,7 @@
 using System.Diagnostics;
 
 //Play();
-FindAndPrintLongestToFindCombination();
+ComputeAndPrintNumberOfGuessesForAllCombinations();
 
 
 
@@ -39,26 +39,57 @@ void Play()
 }
 
 // TODO: put the following method in a better location
-void FindAndPrintLongestToFindCombination()
+void ComputeAndPrintNumberOfGuessesForAllCombinations()
 {
     var stopwatch = new Stopwatch();
+    var histogram = new Dictionary<int, int>();
+    var cpt = 0;
+    var maxSoFar = 0;
+    Combination? argMaxSoFar = null;
+
     stopwatch.Start();
-    var hardestTuple = Solver.AllCombinations.Select(c => (c, ComputeNbIterationsToSolve(c))).MaxBy(t => t.Item2);
+    foreach (var combination in Solver.AllCombinations)
+    {
+        var nbGuesses = ComputeNbIterationsToSolve(combination);
+        cpt++;
+        if (cpt % 10 == 0)
+        {
+            Console.WriteLine($"Done with {cpt} / {6 * 6 * 6 * 6} => {nbGuesses}");
+        }
+
+        if (!histogram.TryAdd(nbGuesses, 1))
+        {
+            histogram[nbGuesses]++;
+        }
+
+        if (nbGuesses > maxSoFar)
+        {
+            maxSoFar = nbGuesses;
+            argMaxSoFar = combination;
+        }
+    }
+
     stopwatch.Stop();
 
     Console.WriteLine("Took: " + stopwatch.ElapsedMilliseconds + "ms");
-    Console.WriteLine($"Hardest combination to find: {hardestTuple.Item1}. Requires {hardestTuple.Item2} iterations");
+    Console.WriteLine($"Hardest combination to find: {argMaxSoFar}. Requires {maxSoFar} iterations");
+    for (var nbGuesses = 1; nbGuesses <= maxSoFar; nbGuesses++)
+    {
+        Console.WriteLine($"{nbGuesses}: {(histogram.TryGetValue(nbGuesses, out var value) ? value : 0)}");
+    }
 }
 
 int ComputeNbIterationsToSolve(Combination solution)
 {
     var nbIterations = 0;
     var playedCombinations = new List<PlayedCombination>();
+    //var solver = new MinMaxSolver(verbose: false, allowGuessKnownToNotBeTheSolution: false);
+    var solver = new DirectSolver(verbose: false);
 
     while (true)
     {
         nbIterations++;
-        var candidate = new DirectSolver(false).ComputeNextGuess(playedCombinations);
+        var candidate = solver.ComputeNextGuess(playedCombinations);
         var result = candidate.ComputeResult(solution);
         if (result.NbAtGoodPosition == 4)
         {
