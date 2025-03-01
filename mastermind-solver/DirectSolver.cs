44873e55c97ac0be7cee3a35efe19bbd1b877c99
Guillaume Turri <g.turri@criteo.com>
2025-03-01 22:35:58 +0100

    Add some doc on the solvers

--- mastermind-solver/DirectSolver.cs
+++ mastermind-solver/DirectSolver.cs
@@ -1,10 +1,15 @@
 ﻿namespace mastermind_solver;
 
-internal class DirectSolver(bool verbose=true, bool useHardCodedFirstGuess=false) : Solver
+/**
+ * Fastest solver to compute the next guess, but it may require up to 9 guesses for some combinations (like "GREEN, BLUE, GREEN, YELLOW", but for 5 others as well).
+ * Setting "useMinMaxFirstGuess" uses the same guess as the min-max solver for the first guess, hence leading to some hybrid solver. If this is set to "true" then in
+ * the worst case if finds the solution in 8 guesses
+ */
+internal class DirectSolver(bool verbose=true, bool useMinMaxFirstGuess=false) : Solver
 {
     public override Combination ComputeNextGuess(List<PlayedCombination> playedCombinations)
     {
-        if (useHardCodedFirstGuess && playedCombinations.Count == 0)
+        if (useMinMaxFirstGuess && playedCombinations.Count == 0)
         {
             return new Combination(Token.BLACK, Token.BLACK, Token.WHITE, Token.WHITE);
         }
--- mastermind-solver/MinMaxSolver.cs
+++ mastermind-solver/MinMaxSolver.cs
@@ -1,5 +1,16 @@
 ﻿namespace mastermind_solver;
 
+/**
+ * Solver which makes a guess that will minimize the number of possible solutions for the next iteration.
+ * <param name="allowGuessKnownToNotBeTheSolution">If set to FALSE then each guess is a possible solution. This reduces a bit the time to compute the next guess, but in the worst case
+ * some solutions (54 of them) will require up to 6 guesses. Though the average number of guesses is 4.50.
+ * If set to TRUE then some guesses will be known to not be a possible solution. This increases a bit the time to compute the next guess, but in the worst case it can only take up to 5
+ * guesses to find the solution. Though the average number of guesses is 4.76
+ * </param>
+ * <param name="usePrecomputations">Should be left to TRUE in order to take the first guesses from a cache -which speeds up a lot this solver. (The only good reason to set it to FALSE)
+ * is to force the re-computation of those guesses, in order to check the content of this cache.
+ * </param>
+ */
 internal class MinMaxSolver(bool verbose=true, bool allowGuessKnownToNotBeTheSolution=false, bool usePrecomputations=true): Solver {
 
     public override Combination ComputeNextGuess(List<PlayedCombination> playedCombinations)
