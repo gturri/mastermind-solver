b0c0b3899de5e6aeb2e7e6e5ebadf963fe964071
Guillaume Turri <g.turri@criteo.com>
2025-05-14 23:05:09 +0200

    Repo has moved

--- mastermind-solver/PlayedCombination.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace mastermind_solver;
-
-public class PlayedCombination(Combination combination, CombinationResult result)
-{
-    public readonly Combination Combination = combination;
-    public readonly CombinationResult Result = result;
-}
